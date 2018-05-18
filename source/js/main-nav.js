92b7dcf87a6b4dd1780b4dbbacd680e27b3555f7
Daelamar <redfess@yandex.ru>
2018-05-19 00:11:38 +0300

    Исправил ошибки от 17.05.2018, отработал эффекты по стайлгайду ( 1 и 2 поле)

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,11 +1,11 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-header__button-menu');
-var navTop = document.querySelector('.main-header__top-nav');
+var header = document.querySelector('.main-header');
 
 navMain.classList.add('main-nav--closed');
-navTop.classList.add('main-header__top-nav--closed');
+header.classList.add('main-header--closed');
 
 navToggle.addEventListener('click', function () {
   navMain.classList.toggle('main-nav--closed');
-  navTop.classList.toggle('main-header__top-nav--closed');
+  header.classList.toggle('main-header--closed');
 });
