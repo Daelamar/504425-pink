e5b86bf5b5570bde5d5d9064ffdd9e50db06e807
Daelamar <redfess@yandex.ru>
2018-05-25 00:28:00 +0300

    Исправил ошибки от 25.05.2018

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,12 +1,16 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-header__button-menu');
-var header = document.querySelector('.main-header');
+var navMain = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".main-header__button-menu");
+var header = document.querySelector(".main-header");
 
-header.classList.remove('main-header--nojs');
-navMain.classList.add('main-nav--closed');
-header.classList.add('main-header--closed');
+[].forEach.call(document.querySelectorAll(".form__legend"), function(item) {
+  item.setAttribute("align", "center");
+});
 
-navToggle.addEventListener('click', function () {
-  navMain.classList.toggle('main-nav--closed');
-  header.classList.toggle('main-header--closed');
+header.classList.remove("main-header--nojs");
+navMain.classList.add("main-nav--closed");
+header.classList.add("main-header--closed");
+
+navToggle.addEventListener("click", function () {
+  navMain.classList.toggle("main-nav--closed");
+  header.classList.toggle("main-header--closed");
 });
